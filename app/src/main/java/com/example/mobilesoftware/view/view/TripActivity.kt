930c51f29ea95bb19edb1bd43e3fcf3b876e3c77
terenzzzz <terence592592@gmail.com>
2022-11-28 19:09:10 +0000

    'idk'

--- app/src/main/java/com/example/mobilesoftware/view/view/TripActivity.kt
+++ app/src/main/java/com/example/mobilesoftware/view/view/TripActivity.kt
@@ -1,7 +1,6 @@
 package com.example.mobilesoftware.view.view
 
 import android.Manifest
-import android.annotation.SuppressLint
 import android.content.Context
 import android.content.Intent
 import android.content.pm.PackageManager
@@ -10,21 +9,24 @@ import android.os.Bundle
 import android.os.Looper
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
-import androidx.core.content.ContextCompat
-import androidx.fragment.app.FragmentManager
-import androidx.fragment.app.FragmentTransaction
+
 import com.example.mobilesoftware.R
 import com.example.mobilesoftware.databinding.ActivityTripBinding
 import com.example.mobilesoftware.view.viewmodels.TripViewModel
 import com.google.android.gms.location.*
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
 
 
-
-class TripActivity : AppCompatActivity() {
+class TripActivity : AppCompatActivity(), OnMapReadyCallback{
 
     var myViewModel = TripViewModel()
 
-    private lateinit var map: MapsFragment
+    private lateinit var mMap: GoogleMap
 
 //    Location
     val PERMISSION_LOCATION_GPS:Int = 1
@@ -48,23 +50,21 @@ class TripActivity : AppCompatActivity() {
         binding.viewModel = myViewModel
         myViewModel.onCreate()
 
-//        Add map
-        map = MapsFragment()
-        val manager: FragmentManager = supportFragmentManager
-        val transaction: FragmentTransaction = manager.beginTransaction()
-        transaction.add(R.id.fl_map, map);
-        transaction.commit();
-
-
+        //        Add map
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        val mapFragment = supportFragmentManager
+            .findFragmentById(R.id.map) as SupportMapFragment
+        mapFragment.getMapAsync(this)
 
 //        GetLocation
-        locationCallback = object : LocationCallback() {
-            override fun onLocationResult(p0: LocationResult) {
-                p0?:return
-                for (location in p0.locations){
+        locationCallback = object : LocationCallback(){
+            override fun onLocationResult(locationResult: LocationResult) {
+                locationResult ?: return
+                for (location in locationResult.locations){
                     lastLocation = location
-                    myViewModel.setLocation(lastLocation.latitude.toString(),lastLocation.longitude.toString())
                 }
+                myViewModel.setLocation(lastLocation.latitude.toString(),lastLocation.longitude.toString())
+                addMarker(lastLocation.latitude,lastLocation.longitude)
             }
         }
         fusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
@@ -86,7 +86,6 @@ class TripActivity : AppCompatActivity() {
         }
     }
 
-
     override fun onPause() {
         super.onPause()
         myViewModel.onPause()
@@ -95,7 +94,6 @@ class TripActivity : AppCompatActivity() {
 
     override fun onResume() {
         super.onResume()
-        myViewModel.onResume()
         refreshLatLon()
     }
 
@@ -105,26 +103,29 @@ class TripActivity : AppCompatActivity() {
     }
 
 
-    @SuppressLint("MissingPermission")
     private fun refreshLatLon(){
-        if (ContextCompat.checkSelfPermission(this,
-                Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-            ActivityCompat.requestPermissions(this,
-                arrayOf(Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION),
-                PERMISSION_LOCATION_GPS)
-        }
-
-        var locationRequest = LocationRequest.Builder(Priority.PRIORITY_HIGH_ACCURACY, Long.MAX_VALUE
-        ).build()
+        if (ActivityCompat.checkSelfPermission(this,Manifest.permission.ACCESS_FINE_LOCATION)!=PackageManager.PERMISSION_GRANTED){
+            ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.ACCESS_FINE_LOCATION,Manifest.permission.ACCESS_COARSE_LOCATION),PERMISSION_LOCATION_GPS)
+        }else{
+            var locationRequest = LocationRequest.Builder(
+                Priority.PRIORITY_HIGH_ACCURACY,
+                10000
+            ).build()
 //        var locationRequest = LocationRequest.create()
-        fusedLocationClient.requestLocationUpdates(locationRequest,locationCallback, Looper.getMainLooper())
+            fusedLocationClient.requestLocationUpdates(locationRequest,locationCallback, Looper.getMainLooper())
+        }
     }
 
-//    override fun onMapReady(p0: GoogleMap) {
-//        val Sheffield = LatLng(53.3847433, -1.4741)
-//        p0.addMarker(MarkerOptions().position(Sheffield).title("Marker in Sheffield"))
-//        p0.moveCamera(CameraUpdateFactory.newLatLngZoom(Sheffield, 15f))
-//    }
+
+    override fun onMapReady(googleMap: GoogleMap) {
+        mMap = googleMap
+    }
+
+    private fun addMarker(latitude:Double,longitude:Double){
+        val point = LatLng(latitude, longitude)
+        mMap.addMarker(MarkerOptions().position(point))
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(point,15f))
+    }
 
 
 }
